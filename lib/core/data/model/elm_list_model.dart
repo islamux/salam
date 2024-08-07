4af9e19ea4a714496d29bfa73c54ba3194adf8d2
islamux <fathi733@gmail.com>
2024-08-07 22:54:33 +0300

    elm text ders sixteen

--- lib/core/data/model/elm_list_model.dart
+++ lib/core/data/model/elm_list_model.dart
@@ -335,10 +335,46 @@ List<ElmModel> elmList16 = [
       elmTextOne6: ElmTextDersSixteen.elmTextDersSixteenOne6,
       ayahHadithOne5: ElmTextDersSixteen.ayahHadithOne5,
       footerOne: ElmTextDersSixteen.footerOne),
+
   // page 2
-  ElmModel(elmText: ElmTextDersSixteen.elmTextDersSixteen2),
+  ElmModel(
+    elmTextTwo1: ElmTextDersSixteen.elmTextDersSixteenTwo1,
+    ayahHadithTwo1: ElmTextDersSixteen.ayahHadithTow1,
+    elmTextTwo2: ElmTextDersSixteen.elmTextDersSixteenTwo2,
+    ayahHadithTwo2: ElmTextDersSixteen.ayahHadithTwo2,
+    elmTextTwo3: ElmTextDersSixteen.elmTextDersSixteenTwo3,
+    ayahHadithTwo3: ElmTextDersSixteen.ayahHadithTwo3,
+    subtitleTwo1: ElmTextDersSixteen.subtitleTwo1,
+    elmTextTwo4: ElmTextDersSixteen.elmTextDersSixteenTwo4,
+    ayahHadithTwo4: ElmTextDersSixteen.ayahHadithTwo4,
+    elmTextTwo5: ElmTextDersSixteen.elmTextDersSixteenTwo5,
+    footerTwo: ElmTextDersSixteen.footerTwo,
+  ),
+
   // page 3
-  ElmModel(elmText: ElmTextDersSixteen.elmTextDersSixteen3),
+  ElmModel(
+      subtitleThree1: ElmTextDersSixteen.subtitleThree3,
+      ayahHadithThree1: ElmTextDersSixteen.ayahHadithThree1,
+      elmTextThree1: ElmTextDersSixteen.elmTextDersSixteenThree1,
+      subtitleThree2: ElmTextDersSixteen.subtitleThree2,
+      elmTextThree2: ElmTextDersSixteen.elmTextDersSixteenThree2,
+      ayahHadithThree2: ElmTextDersSixteen.ayahHadithThree2,
+      subtitleThree3: ElmTextDersSixteen.subtitleThree3,
+      elmTextThree3: ElmTextDersSixteen.elmTextDersSixteenThree3,
+      ayahHadithThree3: ElmTextDersSixteen.ayahHadithThree3,
+      elmTextThree4: ElmTextDersSixteen.elmTextDersSixteenThree4,
+      ayahHadithThree4: ElmTextDersSixteen.ayahHadithThree4,
+      elmTextThree5: ElmTextDersSixteen.elmTextDersSixteenThree5,
+      ayahHadithThree5: ElmTextDersSixteen.ayahHadithThree5,
+      elmTextThree6: ElmTextDersSixteen.elmTextDersSixteenThree6,
+      subtitleThree4: ElmTextDersSixteen.subtitleThree4,
+      ayahHadithThree6: ElmTextDersSixteen.ayahHadithThree6,
+      elmTextThree7: ElmTextDersSixteen.elmTextDersSixteenThree7,
+      subtitleThree5: ElmTextDersSixteen.subtitleThree5,
+      elmTextThree8: ElmTextDersSixteen.elmTextDersSixteenThree8,
+      ayahHadithThree7: ElmTextDersSixteen.ayahHadithThree7,
+      elmTextThree9: ElmTextDersSixteen.elmTextDersSixteenThree9,
+      footer3: ElmTextDersSixteen.footer3),
 ];
 
 //TextDersSevenTeen
--- lib/core/data/model/elm_model.dart
+++ lib/core/data/model/elm_model.dart
@@ -2,6 +2,7 @@ class ElmModel {
   // Proberties
   final String? elmText; // these to avoid errors in all code , remove it later
 
+  // page 1
   final String? titleOne;
   final String? subtitleOne;
   final String? elmTextOne;
@@ -18,10 +19,80 @@ class ElmModel {
   final String? ayahHadithOne5;
   final String? footerOne;
 
+  // page 2
+  final String? elmTextTwo1;
+  final String? ayahHadithTwo1;
+  final String? elmTextTwo2;
+  final String? ayahHadithTwo2;
+  final String? elmTextTwo3;
+  final String? ayahHadithTwo3;
+  final String? subtitleTwo1;
+  final String? elmTextTwo4;
+  final String? ayahHadithTwo4;
+  final String? elmTextTwo5;
+  final String? footerTwo;
+  final String? subtitleThree1;
+  final String? ayahHadithThree1;
+  final String? elmTextThree1;
+  final String? subtitleThree2;
+  final String? elmTextThree2;
+  final String? ayahHadithThree2;
+
+  // page 3
+  final String? subtitleThree3;
+  final String? elmTextThree3;
+  final String? ayahHadithThree3;
+  final String? elmTextThree4;
+  final String? ayahHadithThree4;
+  final String? elmTextThree5;
+  final String? ayahHadithThree5;
+  final String? elmTextThree6;
+  final String? subtitleThree4;
+  final String? ayahHadithThree6;
+  final String? elmTextThree7;
+  final String? subtitleThree5;
+  final String? elmTextThree8;
+  final String? ayahHadithThree9;
+  final String? ayahHadithThree7;
+  final String? elmTextThree9;
+  final String? footer3;
+
   // Constructor (named to make allow rearange them like i want)
   ElmModel({
     this.elmText, // these to avoid errors in all code , remove it later
 
+    this.elmTextTwo1,
+    this.ayahHadithTwo1,
+    this.elmTextTwo2,
+    this.ayahHadithTwo2,
+    this.elmTextTwo3,
+    this.ayahHadithTwo3,
+    this.subtitleTwo1,
+    this.elmTextTwo4,
+    this.ayahHadithTwo4,
+    this.elmTextTwo5,
+    this.footerTwo,
+    this.subtitleThree1,
+    this.ayahHadithThree1,
+    this.elmTextThree1,
+    this.subtitleThree2,
+    this.elmTextThree2,
+    this.ayahHadithThree2,
+    this.subtitleThree3,
+    this.elmTextThree3,
+    this.ayahHadithThree3,
+    this.elmTextThree4,
+    this.ayahHadithThree4,
+    this.elmTextThree5,
+    this.ayahHadithThree5,
+    this.elmTextThree6,
+    this.subtitleThree4,
+    this.ayahHadithThree6,
+    this.elmTextThree7,
+    this.ayahHadithThree7,
+    this.subtitleThree5,
+    this.elmTextThree8,
+    this.ayahHadithThree9,
     this.titleOne,
     this.subtitleOne,
     this.elmTextOne,
@@ -36,6 +107,8 @@ class ElmModel {
     this.ayahHadithOne4,
     this.elmTextOne6,
     this.ayahHadithOne5,
+    this.elmTextThree9,
     this.footerOne,
+    this.footer3,
   });
 }
