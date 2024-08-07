8a27f273804f75f5f23a36c89d8b04fc3390eae5
islamux <fathi733@gmail.com>
2024-08-07 07:57:10 +0300

    update rich-txt

--- lib/core/data/model/elm_model.dart
+++ lib/core/data/model/elm_model.dart
@@ -1,7 +1,41 @@
 class ElmModel {
   // Proberties
-  final String? elmText;
+  final String? elmText; // these to avoid errors in all code , remove it later
 
-  // Constructor
-  ElmModel({this.elmText});
+  final String? titleOne;
+  final String? subtitleOne;
+  final String? elmTextOne;
+  final String? ayahHadithOne;
+  final String? elmTextOne2;
+  final String? subtitleOne2;
+  final String? elmTextOne3;
+  final String? ayahHadithOne2;
+  final String? elmTextOne4;
+  final String? ayahHadithOne3;
+  final String? elmTextOne5;
+  final String? ayahHadithOne4;
+  final String? elmTextOne6;
+  final String? ayahHadithOne5;
+  final String? footerOne;
+
+  // Constructor (named to make allow rearange them like i want)
+  ElmModel({
+    this.elmText, // these to avoid errors in all code , remove it later
+
+    this.titleOne,
+    this.subtitleOne,
+    this.elmTextOne,
+    this.ayahHadithOne,
+    this.elmTextOne2,
+    this.subtitleOne2,
+    this.elmTextOne3,
+    this.ayahHadithOne2,
+    this.elmTextOne4,
+    this.ayahHadithOne3,
+    this.elmTextOne5,
+    this.ayahHadithOne4,
+    this.elmTextOne6,
+    this.ayahHadithOne5,
+    this.footerOne,
+  });
 }
