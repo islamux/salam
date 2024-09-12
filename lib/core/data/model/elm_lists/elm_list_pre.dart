39a8cd0ad950aab3c768f99c448c6489787f8cf4
islamux <fathi733@gmail.com>
2024-09-12 12:03:52 +0300

    first try to edit model

--- lib/core/data/model/elm_lists/elm_list_pre.dart
+++ lib/core/data/model/elm_lists/elm_list_pre.dart
@@ -6,21 +6,26 @@ List<ElmModel> elmListPre = [
   // page 1
   ElmModel(
     title: ElmTextPre.titlePreOne,
-    ayah: ElmTextPre.ayahHadithPreOne_1,
-    text: ElmTextPre.elmTextPreOne_1,
+    ayahs: [ElmTextPre.ayahHadithPreOne_1],
+    texts: [ElmTextPre.elmTextPreOne_1],
   ),
   // page 2
   ElmModel(
     title: ElmTextPre.titlePreTwo,
-    subtitle: ElmTextPre.subtitlePreTwo_1,
-    text: ElmTextPre.elmTextPreTwo_1,
+    subtitles: [ElmTextPre.subtitlePreTwo_1],
+    texts: [ElmTextPre.elmTextPreTwo_1],
   ),
   //page 3
   ElmModel(
-      ayah: ElmTextPre.ayahHadithPreThree_1,
-      text: ElmTextPre.elmTextPreThree_1,
-      ayah2: ElmTextPre.ayahHadithPreThree_2,
-      text2: ElmTextPre.elmTextPreThree_2,
-      ayah3: ElmTextPre.ayahHadithPreThree_3,
-      text3: ElmTextPre.elmTextPreThree_3)
+    ayahs: [
+      ElmTextPre.ayahHadithPreThree_1,
+      ElmTextPre.ayahHadithPreThree_2,
+      ElmTextPre.ayahHadithPreThree_3
+    ],
+    texts: [
+      ElmTextPre.elmTextPreThree_1,
+      ElmTextPre.elmTextPreThree_2,
+      ElmTextPre.elmTextPreThree_3,
+    ],
+  ),
 ];
--- lib/view/widget/custom_text_slider/get_pages_texts/elm_pre/get_page_three_texts.dart
+++ lib/view/widget/custom_text_slider/get_pages_texts/elm_pre/get_page_three_texts.dart
@@ -1,15 +1,41 @@
 import 'package:elm/core/data/model/elm_lists/elm_list_pre.dart';
+import 'package:elm/core/data/model/elm_model.dart';
 import 'package:elm/core/data/static/theme/app_them.dart';
 import 'package:flutter/material.dart';
 
 List<TextSpan> getPageThreeTexts(int i) {
   TextStyle ayah = AppTheme.customTextStyleHadith();
+// to get the elments
+  ElmModel elm = elmListPre[i];
+  // تأكد من أن القوائم تحتوي على 3 عناصر على الأقل، عن طريق ملء القوائم بعناصر فارغة إذا لزم الأمر
+  List<String> ayahs = List.from(elm.ayahs as Iterable)
+    ..addAll(List.filled(3 - elm.ayahs!.length, ''));
+
+  List<String> texts = List.from(elm.texts as Iterable)
+    ..addAll(List.filled(3 - elm.texts!.length, ''));
+
   return [
-    TextSpan(text: elmListPre[i].ayah, style: ayah),
-    TextSpan(text: elmListPre[i].text),
-    TextSpan(text: elmListPre[i].ayah2, style: ayah),
-    TextSpan(text: elmListPre[i].text2),
-    TextSpan(text: elmListPre[i].ayah3, style: ayah),
-    TextSpan(text: elmListPre[i].text3),
+    TextSpan(text: ayahs[0], style: ayah),
+    TextSpan(text: texts[0]),
+    TextSpan(text: ayahs[1], style: ayah),
+    TextSpan(text: texts[1]),
+    TextSpan(text: ayahs[2], style: ayah),
+    TextSpan(text: texts[2]),
   ];
 }
+
+
+
+
+
+// List<TextSpan> getPageThreeTexts(int i) {
+//   TextStyle ayah = AppTheme.customTextStyleHadith();
+//   return [
+//     TextSpan(text: elmListPre[i].ayah, style: ayah),
+//     TextSpan(text: elmListPre[i].text),
+//     TextSpan(text: elmListPre[i].ayah2, style: ayah),
+//     TextSpan(text: elmListPre[i].text2),
+//     TextSpan(text: elmListPre[i].ayah3, style: ayah),
+//     TextSpan(text: elmListPre[i].text3),
+//   ];
+// }
