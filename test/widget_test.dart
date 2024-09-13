df9e8d0c62bf8621643a4b2ed84dddb8907f34e8
islamux <fathi733@gmail.com>
2024-09-13 08:35:39 +0300

    widget_test to test Elm1Controller with BaseController

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,25 +5,26 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:elm/main.dart';
-import 'package:flutter/material.dart';
+import 'package:elm/controller/elm1_controller.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const Elm());
+  late Elm1Controller controller;
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+  setUp(() {
+    // إنشاء instance من Elm1Controller
+    controller = Elm1Controller();
+  });
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+  test('increaseFontSize يجب أن تزيد حجم الخط', () {
+    // التحقق من حجم الخط الابتدائي
+    double initialFontSize = controller.fontSize;
+    expect(initialFontSize, 21.0); // حجم الخط الابتدائي الذي افترضته
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // استدعاء increaseFontSize()
+    controller.increaseFontSize();
+
+    // التحقق من أن حجم الخط قد زاد
+    expect(controller.fontSize, initialFontSize + 2.0);
   });
 }
