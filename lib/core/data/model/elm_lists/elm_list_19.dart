1082efb2cc3ad2f2342661eac6878dcf77377f41
islamux <fathi733@gmail.com>
2024-09-13 23:03:27 +0300

    copy elm19 to code

--- lib/core/data/model/elm_lists/elm_list_19.dart
+++ lib/core/data/model/elm_lists/elm_list_19.dart
@@ -1,14 +1,32 @@
 //TextDersSixTeen
 import 'package:elm/core/data/model/elm_model.dart';
+import 'package:elm/core/data/static/text/elm_text_ders_eight.dart';
+import 'package:elm/core/data/static/text/elm_text_ders_nineteen.dart';
 
 // TextDersSeventeen
 List<ElmModel> elmList19 = [
   // page 1
-  ElmModel(),
-  // page 2
-  ElmModel(),
+  ElmModel(
+    title: ElmTextNineteen.titleNineteenOne,
+    text: ElmTextNineteen.elmTextNineteenOne_1,
+    ayah: ElmTextNineteen.ayahHadithNineteenOne_1,
+  ),
+// page 2
+  ElmModel(
+    subtitle: ElmTextNineteen.subtitleNineteenTwo_1,
+    text2: ElmTextNineteen.elmTextNineteenTwo_1,
+    text3: ElmTextNineteen.elmTextNineteenTwo_2,
+    subtitle2: ElmTextNineteen.subtitleNineteenTwo_2,
+    text4: ElmTextNineteen.elmTextNineteenTwo_3,
+    ayah2: ElmTextNineteen.ayahHadithNineteenTwo_1,
+  ),
+
   // page 3
-  ElmModel(),
+  ElmModel(
+    text: ElmTextNineteen.elmTextNineteenThree_1,
+    ayah: ElmTextNineteen.ayahHadithNineteenThree_1,
+    text2: ElmTextNineteen.elmTextNineteenThree_2,
+  ),
   // page 4
   ElmModel(),
   // page 5
@@ -22,5 +40,17 @@ List<ElmModel> elmList19 = [
   // page 9
   ElmModel(),
   // page 10
+  ElmModel(),
+  // page 11
+  ElmModel(),
+  // page 12
+  ElmModel(),
+// page 13
+  ElmModel(),
+// page 14
+  ElmModel(),
+// page 15
+  ElmModel(),
+// page 16
   ElmModel(),
 ];
