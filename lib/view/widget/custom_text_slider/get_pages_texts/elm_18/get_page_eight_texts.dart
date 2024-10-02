95c7bf05e712f7603f2d71e9bc485c417cb9b334
islamux <fathi733@gmail.com>
2024-10-02 17:43:08 +0300

    finished elm17, 18, 19, working on 20

--- lib/view/widget/custom_text_slider/get_pages_texts/elm_18/get_page_eight_texts.dart
+++ lib/view/widget/custom_text_slider/get_pages_texts/elm_18/get_page_eight_texts.dart
@@ -1,15 +1,9 @@
 import 'package:elm/core/data/model/elm_lists/elm_list_18.dart';
 import 'package:elm/core/data/model/elm_model.dart';
-import 'package:elm/core/data/static/theme/app_them.dart';
+import 'package:elm/view/widget/custom_text_span.dart';
 import 'package:flutter/material.dart';
 
 List<TextSpan> getPageEightTexts(int i) {
-  TextStyle ayahStyle = AppTheme.customTextStyleHadith();
-  TextStyle subtitleStyle = AppTheme.customTextStyleSubtitle();
-  TextStyle titleStyle = AppTheme.customTextStyleTitle();
   ElmModel elm = elmList18[i];
-  return [
-    TextSpan(text: elm.text),
-    TextSpan(text: elm.subtitle, style: subtitleStyle),
-  ];
+  return [customTextSpan(elm.text, null)];
 }
