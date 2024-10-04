da57896dd81ee334e942a22763e30b4d0dcb0302
islamux <fathi733@gmail.com>
2024-10-04 22:04:53 +0300

    convert model variables to list

---
+++ lib/core/data/model/elm_lists/elm_list_14_new.dart
@@ -0,0 +1,181 @@
+//TexDerseFourteen
+import 'package:elm/core/data/static/text/elm_text_ders_fourteen.dart';
+
+import '../elm_model_new.dart';
+
+List<ElmModelNew> elmList14New = [
+  // page 1
+  ElmModelNew(
+    titles: [
+      ElmTextDersFourteen.titleFourteenOne,
+    ],
+    texts: [
+      ElmTextDersFourteen.elmTextFourteenOne_1,
+    ],
+  ),
+  // page 2
+  ElmModelNew(
+    texts: [
+      ElmTextDersFourteen.elmTextFourteenTwo_1,
+      ElmTextDersFourteen.elmTextFourteenTwo_2,
+    ],
+    ayahs: [
+      ElmTextDersFourteen.ayahHadithFourteenTwo_1,
+    ],
+  ),
+
+  // page 3
+  ElmModelNew(
+    subtitles: [
+      ElmTextDersFourteen.subtitleFourteenThree_1,
+    ],
+    ayahs: [
+      ElmTextDersFourteen.ayahHadithFourteenThree_1,
+      ElmTextDersFourteen.ayahHadithFourteenThree_2,
+    ],
+    texts: [
+      ElmTextDersFourteen.elmTextFourteenThree_1,
+      ElmTextDersFourteen.elmTextFourteenThree_2,
+    ],
+  ),
+
+  // page 4
+  ElmModelNew(
+    texts: [
+      ElmTextDersFourteen.elmTextFourteenFour_1,
+      ElmTextDersFourteen.elmTextFourteenFour_2,
+    ],
+    ayahs: [
+      ElmTextDersFourteen.ayahHadithFourteenFour_1,
+      ElmTextDersFourteen.ayahHadithFourteenFour_2
+    ],
+  ),
+  // page 5
+  ElmModelNew(
+    texts: [
+      ElmTextDersFourteen.elmTextFourteenFive_1,
+    ],
+    ayahs: [
+      ElmTextDersFourteen.ayahHadithFourteenFive_1,
+    ],
+  ),
+  // page 6
+  ElmModelNew(
+    texts: [
+      ElmTextDersFourteen.elmTextFourteenSix_1,
+      ElmTextDersFourteen.elmTextFourteenSix_2,
+      ElmTextDersFourteen.elmTextFourteenSix_3,
+      ElmTextDersFourteen.elmTextFourteenSix_4,
+      ElmTextDersFourteen.elmTextFourteenSix_5,
+      ElmTextDersFourteen.elmTextFourteenSix_6,
+    ],
+    ayahs: [
+      ElmTextDersFourteen.ayahHadithFourteenSix_1,
+      ElmTextDersFourteen.ayahHadithFourteenSix_2,
+      ElmTextDersFourteen.ayahHadithFourteenSix_3,
+      ElmTextDersFourteen.ayahHadithFourteenSix_4,
+      ElmTextDersFourteen.ayahHadithFourteenSix_5,
+    ],
+  ),
+
+  // page 7
+  ElmModelNew(
+    ayahs: [
+      ElmTextDersFourteen.ayahHadithFourteenSeven_1,
+      ElmTextDersFourteen.ayahHadithFourteenSeven_2,
+      ElmTextDersFourteen.ayahHadithFourteenSeven_3,
+    ],
+    texts: [
+      ElmTextDersFourteen.elmTextFourteenSeven_1,
+      ElmTextDersFourteen.elmTextFourteenSeven_2,
+      ElmTextDersFourteen.elmTextFourteenSeven_3,
+    ],
+    subtitles: [
+      ElmTextDersFourteen.subtitleFourteenSeven_1,
+    ],
+  ),
+
+  // page 8
+  ElmModelNew(
+    texts: [
+      ElmTextDersFourteen.elmTextFourteenEight_1,
+      ElmTextDersFourteen.elmTextFourteenEight_2,
+    ],
+    ayahs: [
+      ElmTextDersFourteen.ayahHadithFourteenEight_1,
+    ],
+  ),
+
+  // page 9
+  ElmModelNew(
+    titles: [
+      ElmTextDersFourteen.titleFourteenNine_1,
+    ],
+    ayahs: [
+      ElmTextDersFourteen.ayahHadithFourteenNine_1,
+    ],
+    texts: [
+      ElmTextDersFourteen.elmTextFourteenNine_1,
+    ],
+  ),
+  // page 10
+  ElmModelNew(
+    ayahs: [
+      ElmTextDersFourteen.ayahHadithFourteenTen_1,
+      ElmTextDersFourteen.ayahHadithFourteenTen_2,
+      ElmTextDersFourteen.ayahHadithFourteenTen_3,
+      ElmTextDersFourteen.ayahHadithFourteenTen_4,
+    ],
+    texts: [
+      ElmTextDersFourteen.elmTextFourteenTen_1,
+      ElmTextDersFourteen.elmTextFourteenTen_2,
+      ElmTextDersFourteen.elmTextFourteenTen_3,
+      ElmTextDersFourteen.elmTextFourteenTen_4,
+    ],
+  ),
+
+  // page 11
+  ElmModelNew(
+    ayahs: [
+      ElmTextDersFourteen.ayahHadithFourteenEleven_1,
+    ],
+    texts: [
+      ElmTextDersFourteen.elmTextFourteenEleven_1,
+    ],
+  ),
+  // page 12
+  ElmModelNew(
+    titles: [],
+    ElmTextDersFourteen.titleFourteenTwelve_1,
+    texts: [],
+    ElmTextDersFourteen.elmTextFourteenTwelve_1,
+  ),
+  // page 13
+  ElmModelNew(
+    texts: [],
+    ElmTextDersFourteen.elmTextFourteenTherteen_1,
+  ),
+  // page 14
+  ElmModelNew(
+    ayahs: [],
+    ElmTextDersFourteen.ayahHadithFourteenFourteen_1,
+    texts: [],
+    ElmTextDersFourteen.elmTextFourteenFourteen_1,
+  ),
+  // page 15
+  ElmModelNew(
+    ayahs: [],
+    ElmTextDersFourteen.ayahHadithFourteenFifteen_1,
+    texts: [],
+    ElmTextDersFourteen.elmTextFourteenFifteen_1,
+    ayah2: ElmTextDersFourteen.ayahHadithFourteenFifteen_2,
+    text2: ElmTextDersFourteen.elmTextFourteenFifteen_2,
+  ),
+  // page 16
+  ElmModelNew(
+    ayahs: [],
+    ElmTextDersFourteen.ayahHadithFourteenSixteen_1,
+    texts: [],
+    ElmTextDersFourteen.elmTextFourteenSixteen_1,
+  ),
+];
--- lib/core/data/model/elm_lists/elm_list_9_new.dart
+++ lib/core/data/model/elm_lists/elm_list_9_new.dart
@@ -3,70 +3,86 @@ import 'package:elm/core/data/static/text/elm_text_ders_nine.dart';
 
 import '../elm_model_new.dart';
 
-List<ElmModelNew> elmList9New= [
+List<ElmModelNew> elmList9New = [
   // 1
-  ElmModelNew(
-    ayahs:[] ElmTextDersNine.ayahHadithNineOne_1,
-    texts:[] ElmTextDersNine.elmTextNineOne_1,
-    ayah2: ElmTextDersNine.ayahHadithNineOne_2,
-  ),
+  ElmModelNew(ayahs: [
+    ElmTextDersNine.ayahHadithNineOne_1,
+    ElmTextDersNine.ayahHadithNineOne_2,
+  ], texts: [
+    ElmTextDersNine.elmTextNineOne_1,
+  ]),
   // 2
-  ElmModelNew(
-    texts:[] ElmTextDersNine.elmTextNineTwo_1,
-    ayahs:[] ElmTextDersNine.ayahHadithNineTwo_1,
-  ),
+  ElmModelNew(texts: [
+    ElmTextDersNine.elmTextNineTwo_1,
+  ], ayahs: [
+    ElmTextDersNine.ayahHadithNineTwo_1,
+  ]),
   // 3
-  ElmModelNew(
-    texts:[] ElmTextDersNine.elmTextNineThree_1,
-    ayahs:[] ElmTextDersNine.ayahHadithNineThree_1,
-  ),
+  ElmModelNew(texts: [
+    ElmTextDersNine.elmTextNineThree_1,
+  ], ayahs: [
+    ElmTextDersNine.ayahHadithNineThree_1,
+  ]),
   // 4
-  ElmModelNew(
-    texts:[] ElmTextDersNine.elmTextNineFour_1,
-    ayahs:[] ElmTextDersNine.ayahHadithNineFour_1,
-  ),
+  ElmModelNew(texts: [
+    ElmTextDersNine.elmTextNineFour_1,
+  ], ayahs: [
+    ElmTextDersNine.ayahHadithNineFour_1,
+  ]),
   // 5
-  ElmModelNew(
-    texts:[] ElmTextDersNine.elmTextNineFive_1,
-    ayahs:[] ElmTextDersNine.ayahHadithNineFive_1,
-    text2: ElmTextDersNine.elmTextNineFive_2,
-  ),
+  ElmModelNew(texts: [
+    ElmTextDersNine.elmTextNineFive_1,
+    ElmTextDersNine.elmTextNineFive_2,
+  ], ayahs: [
+    ElmTextDersNine.ayahHadithNineFive_1,
+  ]),
   // 6
-  ElmModelNew(
-    texts:[] ElmTextDersNine.elmTextNineSix_1,
-    ayahs:[] ElmTextDersNine.ayahHadithNineSix_1,
-    text2: ElmTextDersNine.elmTextNineSix_2,
-    ayah2: ElmTextDersNine.ayahHadithNineSix_2,
-    text3: ElmTextDersNine.elmTextNineSix_3,
-  ),
+  ElmModelNew(texts: [
+    ElmTextDersNine.elmTextNineSix_1,
+    ElmTextDersNine.elmTextNineSix_2,
+    ElmTextDersNine.elmTextNineSix_3,
+  ], ayahs: [
+    ElmTextDersNine.ayahHadithNineSix_1,
+    ElmTextDersNine.ayahHadithNineSix_2,
+  ]),
   // 7
-  ElmModelNew(
-    texts:[] ElmTextDersNine.elmTextNineSeven_1,
-    ayahs:[] ElmTextDersNine.ayahHadithNineSeven_1,
-    text2: ElmTextDersNine.elmTextNineSeven_2,
-  ),
+  ElmModelNew(texts: [
+    ElmTextDersNine.elmTextNineSeven_1,
+    ElmTextDersNine.elmTextNineSeven_2,
+  ], ayahs: [
+    ElmTextDersNine.ayahHadithNineSeven_1,
+  ]),
   // 8
-  ElmModelNew(
-    texts:[] ElmTextDersNine.elmTextNineEight_1,
-    ayahs:[] ElmTextDersNine.ayahHadithNineEight_1,
-    text2: ElmTextDersNine.elmTextNineEight_2,
-  ),
+  ElmModelNew(texts: [
+    ElmTextDersNine.elmTextNineEight_1,
+    ElmTextDersNine.elmTextNineEight_2,
+  ], ayahs: [
+    ElmTextDersNine.ayahHadithNineEight_1,
+  ]),
   // 9
   ElmModelNew(
-    titles:[] ElmTextDersNine.titleNineNine,
-    texts:[] ElmTextDersNine.elmTextNineNine_1,
-    ayahs:[] ElmTextDersNine.ayahHadithNineNine_1,
-    text2: ElmTextDersNine.elmTextNineNine_2,
-    ayah2: ElmTextDersNine.ayahHadithNineNine_2,
-    text3: ElmTextDersNine.elmTextNineNine_3,
+    titles: [
+      ElmTextDersNine.titleNineNine,
+    ],
+    texts: [
+      ElmTextDersNine.elmTextNineNine_1,
+      ElmTextDersNine.elmTextNineNine_2,
+      ElmTextDersNine.elmTextNineNine_3,
+    ],
+    ayahs: [
+      ElmTextDersNine.ayahHadithNineNine_1,
+      ElmTextDersNine.ayahHadithNineNine_2,
+    ],
     footer: ElmTextDersNine.footerNineNine,
   ),
   // 10
-  ElmModelNew(
-    titles:[] ElmTextDersNine.titleNineTen_1,
-    texts:[] ElmTextDersNine.elmTextNineTen_1,
-    ayahs:[] ElmTextDersNine.ayahHadithNineTen_1,
-    text2: ElmTextDersNine.elmTextNineTen_2,
-    ayah2: ElmTextDersNine.ayahHadithNineTen_2,
-  ),
+  ElmModelNew(titles: [
+    ElmTextDersNine.titleNineTen_1,
+  ], texts: [
+    ElmTextDersNine.elmTextNineTen_1,
+    ElmTextDersNine.elmTextNineTen_2,
+  ], ayahs: [
+    ElmTextDersNine.ayahHadithNineTen_1,
+    ElmTextDersNine.ayahHadithNineTen_2,
+  ]),
 ];
