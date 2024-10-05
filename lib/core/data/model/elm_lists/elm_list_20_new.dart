2e2c4e37f2359681789c2e06d74e71369921a299
islamux <fathi733@gmail.com>
2024-10-05 11:18:07 +0300

    refactor:finished elmList20New

---
+++ lib/core/data/model/elm_lists/elm_list_20_new.dart
@@ -0,0 +1,260 @@
+//TextDersSixTeen
+import 'package:elm/core/data/model/elm_model.dart';
+import 'package:elm/core/data/static/text/elm_text_ders_twenty.dart';
+
+// TextDersSeventeen
+List<ElmModelNew> elmList20New = [
+  // page 1
+  ElmModelNew(
+    texts:[ElmTextTwenty.elmTextTwentyOne_1,ElmTextTwenty.elmTextTwentyOne_2,ElmTextTwenty.elmTextTwentyOne_3,], 
+    titles:[ElmTextTwenty.titleTwentyOne,], 
+    ayahs:[ElmTextTwenty.ayahHadithTwentyOne1,ElmTextTwenty.ayahHadithTwentyOne_2,], 
+    subtitles:[ElmTextTwenty.subtitleTwentyOne_1,], 
+  ),
+
+// page 2
+  ElmModelNew(
+    subtitles:[ElmTextTwenty.subtitleTwentyTwo_1,], 
+    texts:[ElmTextTwenty.elmTextTwentyTwo_1,ElmTextTwenty.elmTextTwentyTwo_2,], 
+    ayahs:[ElmTextTwenty.ayahHadithTwentyTwo_1,], 
+  ),
+  // page 3
+  ElmModelNew(
+    subtitles:[ElmTextTwenty.subtitleTwentyThree_1,], 
+    texts:[ElmTextTwenty.elmTextTwentyThree_1,ElmTextTwenty.elmTextTwentyThree_2,], 
+    ayahs:[ElmTextTwenty.ayahHadithTwentyThree_1,], 
+  ),
+  // page 4
+  ElmModelNew(
+    subtitles:[ElmTextTwenty.subtitleTwentyFour_1,ElmTextTwenty.subtitleTwentyFour_2], 
+    texts:[ElmTextTwenty.elmTextTwentyFour_1,ElmTextTwenty.elmTextTwentyFour_2,], 
+    ayahs:[ElmTextTwenty.ayahHadithTwentyFour_1,], 
+  ),
+  // page 5
+  ElmModelNew(
+    texts:[ElmTextTwenty.elmTextTwentyFive_1,ElmTextTwenty.elmTextTwentyFive_2,ElmTextTwenty.elmTextTwentyFive_3,ElmTextTwenty.elmTextTwentyFive_4,], 
+    ayahs:[ElmTextTwenty.ayahHadithTwentyFive_1,ElmTextTwenty.ayahHadithTwentyFive_2,ElmTextTwenty.ayahHadithTwentyFive_3,ElmTextTwenty.ayahHadithTwentyFive_4,], 
+  ),
+
+  // page 6
+  ElmModelNew(
+    texts:[ElmTextTwenty.elmTextTwentySix_1,ElmTextTwenty.elmTextTwentySix_2,ElmTextTwenty.ayahHadithTwentySix_2,ElmTextTwenty.elmTextTwentySix_3,ElmTextTwenty.elmTextTwentySix_4,], 
+    ayahs:[ElmTextTwenty.ayahHadithTwentySix_1,ElmTextTwenty.ayahHadithTwentySix_3,], 
+  ),
+  // page 7
+  ElmModelNew(
+    ayahs:[ElmTextTwenty.ayahHadithTwentySeven_1,ElmTextTwenty.ayahHadithTwentySeven_2,ElmTextTwenty.ayahHadithTwentySeven_3,], 
+    texts:[ElmTextTwenty.elmTextTwentySeven_1,ElmTextTwenty.elmTextTwentySeven_2,], 
+  ),
+  // page 8
+  ElmModelNew(
+    subtitles:[ElmTextTwenty.subtitleTwentyEight_1,], 
+    texts:[ElmTextTwenty.elmtTextTwentyEight_1,], 
+  ),
+  // page 9
+  ElmModelNew(
+    ayahs:[ElmTextTwenty.ayahHadithTwentyNine_1,ElmTextTwenty.ayahHadithTwentyNine_2,], 
+    texts:[ElmTextTwenty.elmTextTwentyNine_1,], 
+  ),
+  // page 10
+  ElmModelNew(
+    titles:[ElmTextTwenty.titletwentyten,], 
+    texts:[ElmTextTwenty.elmTextTwentyTen_1,], 
+  ),
+  // page 11
+  ElmModelNew(
+    titles:[ElmTextTwenty.titleTwentyEleven,], 
+    texts:[ElmTextTwenty.elmTextTwentyEleven_1,ElmTextTwenty.elmTextTwentyEleven_2,], 
+    ayahs:[ElmTextTwenty.ayahHadithTwentyEleven_1,], 
+  ),
+  // page 12
+  ElmModelNew(
+    texts:[ElmTextTwenty.elmTextTwentyTwelve_1,ElmTextTwenty.elmTextTwentyTwelve_2,], 
+    ayahs:[ElmTextTwenty.ayahHadithTwentyTwelve_1,], 
+  ),
+// page 13
+  ElmModelNew(
+    texts:[ElmTextTwenty.elmTextTwentyTherteen_1,ElmTextTwenty.elmTextTwentyTherteen_2,ElmTextTwenty.elmTextTwentyeTherteen_3,], 
+    subtitles:[ElmTextTwenty.subtitleTwentyTherteen_1,], 
+    ayahs:[ElmTextTwenty.ayahHadithTwentyTherteen_1,], 
+  ),
+// page 14
+  ElmModelNew(
+    ayahs:[ElmTextTwenty.ayahHadithTwentyFourteen_1,], 
+    texts:[ElmTextTwenty.elmTextTwentyFourteen_1,ElmTextTwenty.elmTextTwentyFourteen_2,], 
+  ),
+// page 15
+  ElmModelNew(
+    texts:[ElmTextTwenty.elmTextTwentyFifteen_1,ElmTextTwenty.elmTextTwentyFifteen_2,], 
+    ayahs:[ElmTextTwenty.ayahHadithTwentyFifteen_1,], 
+  ),
+
+// page 16
+  ElmModelNew(
+    titles:[], ElmTextTwenty.titleTwentySixteen,
+    texts:[], ElmTextTwenty.elmTextTwentySixteen_2,
+  ),
+
+// page 17
+  ElmModelNew(
+    texts:[], ElmTextTwenty.elmTextTwentySeventeen_1,
+    ayahs:[], ElmTextTwenty.ayahHdithTwentySeventeen_1,
+  ),
+
+// page 18
+  ElmModelNew(
+    texts:[], ElmTextTwenty.elmTextTwentyEighteen_1,
+  ),
+
+// page 19
+  ElmModelNew(
+    titles:[], ElmTextTwenty.titleTwentyNinteen,
+    texts:[], ElmTextTwenty.elmTextTwentyNinteen_1,
+    ayahs:[], ElmTextTwenty.ayahHadithTwentyNinteen_1,
+  ),
+
+// page 20
+  ElmModelNew(
+    texts:[], ElmTextTwenty.elmTextTwentyTwenty_1,
+    ayahs:[], ElmTextTwenty.ayahHadithTwentyTwenty_1,
+    text2: ElmTextTwenty.elmTextTwentyTwenty_2,
+    ayah2: ElmTextTwenty.ayahHadithTwentyTwenty_2,
+    subtitles:[], ElmTextTwenty.subtitleTwentyTwenty_1,
+    text3: ElmTextTwenty.elmTextTwentyTwenty_3,
+    ayah3: ElmTextTwenty.ayahHadithTwentyTwenty_3,
+  ),
+
+// page 21
+  ElmModelNew(
+    texts:[], ElmTextTwenty.elmTextTwentyTwentyOne_1,
+    ayahs:[], ElmTextTwenty.ayahHadithTwentyTwentyOne_1,
+    text2: ElmTextTwenty.elmTextTwentyTwentyOne_2,
+    subtitles:[], ElmTextTwenty.subtitleTwentyOne_1,
+    text3: ElmTextTwenty.elmTextTwentyTwentyOne_3,
+  ),
+
+// page 22
+  ElmModelNew(
+    texts:[], ElmTextTwenty.elmTextTwentyTwentyTwo_1,
+    ayahs:[], ElmTextTwenty.ayahHadithTwentyTwentyTwo_1,
+    text2: ElmTextTwenty.elmTextTwentyTwentyTwo_2,
+  ),
+
+// page 23
+  ElmModelNew(
+    subtitles:[], ElmTextTwenty.subtitleTwentyTwentyThree_1,
+    texts:[], ElmTextTwenty.elmTextTwentyTwentyThree_1,
+    ayahs:[], ElmTextTwenty.ayahHadithTwentyTwentyThree_1,
+    text2: ElmTextTwenty.elmTextTwentyTwentyThree_2,
+    ayah2: ElmTextTwenty.ayahHadithTwentyTwentyThree_2,
+    text3: ElmTextTwenty.elmTextTwentyTwentyThree_3,
+    ayah3: ElmTextTwenty.ayahHadithTwentyTwentyThree_3,
+  ),
+
+// page 24
+  ElmModelNew(
+    texts:[], ElmTextTwenty.elmTextTwentyTwentyFour_1,
+    ayahs:[], ElmTextTwenty.ayahHadithTwentyTwentyFour_1,
+    text2: ElmTextTwenty.elmTextTwentyTwentyFour_2,
+    ayah2: ElmTextTwenty.ayahHadithTwentyTwentyFour_2,
+  ),
+
+// page 25
+  ElmModelNew(
+    subtitles:[], ElmTextTwenty.subtitleTwentyTwentyFive_1,
+    texts:[], ElmTextTwenty.elmTextTwentyTwentyFive_2,
+  ),
+
+// page 26
+  ElmModelNew(
+    ayahs:[], ElmTextTwenty.ayahHadithTwentyTwentySix_1,
+  ),
+
+// page 27
+  ElmModelNew(
+    subtitles:[], ElmTextTwenty.subtitleTwentyTwentySeven_1,
+    texts:[], ElmTextTwenty.elmTextTwentyTwentySeven_1,
+    ayahs:[], ElmTextTwenty.ayahHadithTwentyTwentySeven_1,
+    text2: ElmTextTwenty.elmTextTwentyTwentySeven_2,
+    ayah2: ElmTextTwenty.ayahHadithTwentyTwentySeven_2,
+    text3: ElmTextTwenty.elmTextTwentyTwentySeven_3,
+    ayah3: ElmTextTwenty.ayahHadithTwentyTwentySeven_3,
+  ),
+
+// page 28
+  ElmModelNew(
+    texts:[], ElmTextTwenty.elmTextTwentyTwentyEight_1,
+    ayahs:[], ElmTextTwenty.ayahHadithTwentyTwentyEight_1,
+    text2: ElmTextTwenty.elmTextTwentyTwentyEight_2,
+    ayah2: ElmTextTwenty.ayahHadithTwentyTwentyEight_2,
+    text3: ElmTextTwenty.elmTextTwentyTwentyEight_3,
+    ayah3: ElmTextTwenty.ayahHadithTwentyTwentyEight_3,
+  ),
+
+// page 29
+  ElmModelNew(
+    texts:[], ElmTextTwenty.elmTextTwentyTwentyNine_1,
+    ayahs:[], ElmTextTwenty.ayahHadithTwentyTwentyNine_1,
+    text2: ElmTextTwenty.elmTextTwentyTwentyNine_2,
+    ayah2: ElmTextTwenty.ayahHadithTwentyTwentyNine_2,
+    text3: ElmTextTwenty.elmTextTwentyTwentyNine_3,
+    ayah3: ElmTextTwenty.ayahHadithTwentyTwentyNine_3,
+    text4: ElmTextTwenty.elmTextTwentyTwentyNine_4,
+    ayah4: ElmTextTwenty.ayahHadithTwentyTwentyNine_4,
+    text5: ElmTextTwenty.elmTextTwentyTwentyNine_5,
+    ayah5: ElmTextTwenty.ayahHadithTwentyTwentyNine_5,
+    text6: ElmTextTwenty.elmTextTwentyTwentyNine_6,
+    ayah6: ElmTextTwenty.ayahHadithTwentyTwentyNine_6,
+    text7: ElmTextTwenty.elmTextTwentyTwentyNine_7,
+    ayah7: ElmTextTwenty.ayahHadithTwentyTwentyNine_7,
+    text8: ElmTextTwenty.elmTextTwentyTwentyNine_8,
+    ayah8: ElmTextTwenty.ayahHadithTwentyTwentyNine_8,
+  ),
+
+// page 30
+  ElmModelNew(
+    texts:[], ElmTextTwenty.elmTextTwentyTherty_1,
+    ayahs:[], ElmTextTwenty.ayahHadithTwentyTherty_1,
+    text2: ElmTextTwenty.elmTextTwentyTherty_2,
+    ayah2: ElmTextTwenty.ayahHadithTwentyTherty_2,
+    text3: ElmTextTwenty.elmTextTwentyTherty_3,
+  ),
+  // page 31
+  ElmModelNew(
+    ayahs:[], ElmTextTwenty.ayahHadithTwentyThertyOne_1,
+    texts:[], ElmTextTwenty.elmTextTwentyThertyOne_1,
+    ayah2: ElmTextTwenty.ayahHadithTwentyThertyOne_2,
+    text2: ElmTextTwenty.elmTextTwentyThertyOne_2,
+    ayah3: ElmTextTwenty.ayahHadithTwentyThertyOne_3,
+  ),
+  // page 32
+  ElmModelNew(
+    texts:[], ElmTextTwenty.elmTextTwentyThertyTwo_1,
+  ),
+// page 33
+  ElmModelNew(
+    texts:[], ElmTextTwenty.elmTextTwentyThertyThree_1,
+    ayahs:[], ElmTextTwenty.ayahHadithTwentyThertyThree_1,
+    subtitles:[], ElmTextTwenty.subtitleTwentyThertyThree_1,
+    text2: ElmTextTwenty.elmTextTwentyThertyThree_2,
+    subtitle2: ElmTextTwenty.subtitleTwentyThertyThree_2,
+    text3: ElmTextTwenty.elmTextTwentyThertyThree_3,
+  ),
+
+// page 34
+  ElmModelNew(
+    ayahs:[], ElmTextTwenty.ayahHadithTwentyThertyFour_1,
+    texts:[], ElmTextTwenty.elmTextTwentyThertyFour_1,
+    ayah2: ElmTextTwenty.ayahHadithTwentyThertyFour_2,
+    text2: ElmTextTwenty.elmTextTwentyThertyFour_2,
+    ayah3: ElmTextTwenty.ayahHadithTwentyThertyFour_3,
+    text3: ElmTextTwenty.elmTextTwentyThertyFour_3,
+  ),
+// page 35
+  ElmModelNew(
+    ayahs:[], ElmTextTwenty.ayahHadithTwentyThertyFive_1,
+    texts:[], ElmTextTwenty.elmTextTwentyThertyFive_1,
+    ayah2: ElmTextTwenty.ayahHadithTwentyThertyFive_2,
+    text2: ElmTextTwenty.elmTextTwentyThertyFive_2,
+  ),
+];
