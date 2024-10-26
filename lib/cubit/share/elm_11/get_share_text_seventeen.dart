3bfb9116e4573ea397326fd19503c71328c5eba8
islamux <fathi733@gmail.com>
2024-10-26 07:22:29 +0300

    refactor: cubit10,11,12

---
+++ lib/cubit/share/elm_11/get_share_text_seventeen.dart
@@ -0,0 +1,10 @@
+import 'package:elm/core/data/model/elm_model_new.dart';
+import 'package:flutter/material.dart';
+
+List<Text> getShareTextSeventeen(
+    int currentPageIndex, List<ElmModelNew> elmList) {
+  ElmModelNew text = elmList[currentPageIndex];
+  return [
+    Text(text.texts![0]),
+  ];
+}
---
+++ lib/cubit/share/elm_11/get_share_text_twenty.dart
@@ -0,0 +1,9 @@
+import 'package:elm/core/data/model/elm_model_new.dart';
+import 'package:flutter/material.dart';
+
+List<Text> getShareTextTwenty(int currentPageIndex, List<ElmModelNew> elmList) {
+  ElmModelNew text = elmList[currentPageIndex];
+  return [
+    Text(text.texts![0]),
+  ];
+}
