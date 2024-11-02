174a962e3e0593355951456fae9d72fc34f32891
islamux <fathi733@gmail.com>
2024-11-02 09:05:20 +0300

    uplaod

--- lib/view/widget/custom_text_slider/get_pages_texts/elm_14_new/get_page_fifteen_texts.dart
+++ lib/view/widget/custom_text_slider/get_pages_texts/elm_14_new/get_page_fifteen_texts.dart
@@ -10,16 +10,16 @@ List<TextSpan> getPageF//ifteenTexts(int i, List<ElmModelNew> elmList) {
   return [
     // ayah 0
     //if (elm.ayahs != null && elm.ayahs!.isNotEmpty)
-      TextSpan(text: elm.ayahs![0], style: ayahStyle),
+      TextSpan(text = elm.ayahs![0], style = ayahStyle),
     // text 0
     //if (elm.texts != null && elm.texts!.isNotEmpty)
-      TextSpan(text: elm.texts![0], style: null),
+      TextSpan(text = elm.texts![0], style = null),
 
     // ayah 1
     //if (elm.ayahs != null && elm.ayahs!.length > 1)
-      TextSpan(text: elm.ayahs![1], style: ayahStyle),
+      TextSpan(text = elm.ayahs![1], style = ayahStyle),
     // text 1
     //if (elm.texts != null && elm.texts!.length > 1)
-      TextSpan(text: elm.texts![1], style: null),
+      TextSpan(text = elm.texts![1], style = null),
   ];
 }
