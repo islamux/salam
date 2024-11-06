5428b3087bb147d35d58bc2dcc3b68189ee9f36e
islamux <fathi733@gmail.com>
2024-11-06 06:42:27 +0300

    refactor:elm_list_22

---
+++ lib/helpers/spacing.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+SizedBox verticalSpacing(double hight) => SizedBox(height: hight.h);
+SizedBox horizentalSpacing(double width) => SizedBox(width: width.h);
