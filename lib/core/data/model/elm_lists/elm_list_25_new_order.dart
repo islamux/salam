adb5a6feb260d620304dd7888deb2af6964cd1e7
islamux <fathi733@gmail.com>
2024-11-23 21:34:11 +0300

    add elm_text_ders_twenty_five.dart

---
+++ lib/core/data/model/elm_lists/elm_list_25_new_order.dart
@@ -0,0 +1,31 @@
+import 'package:elm/core/data/model/elm_model_new_order.dart';
+
+List<ElmModelNewOrder> elmList25NewOrder = [
+  // PageOne
+  ElmModelNewOrder(subtitles: [], texts: [], ayahs: [], order: []),
+  // PageTwo
+  ElmModelNewOrder(subtitles: [], texts: [], ayahs: [], order: []),
+  // PageThree
+  ElmModelNewOrder(subtitles: [], texts: [], ayahs: [], order: []),
+  // PageFour
+  ElmModelNewOrder(subtitles: [], texts: [], ayahs: [], order: []),
+  // PageFive
+  ElmModelNewOrder(subtitles: [], texts: [], ayahs: [], order: []),
+  // PageSix
+  ElmModelNewOrder(subtitles: [], texts: [], ayahs: [], order: []),
+  // PageSeven
+  ElmModelNewOrder(subtitles: [], texts: [], ayahs: [], order: []),
+  // PageEight
+  ElmModelNewOrder(subtitles: [], texts: [], ayahs: [], order: []),
+  // PageNine
+  ElmModelNewOrder(subtitles: [], texts: [], ayahs: [], order: []),
+  // PageTen
+  ElmModelNewOrder(subtitles: [], texts: [], ayahs: [], order: []),
+  // PageEleven
+  ElmModelNewOrder(subtitles: [], texts: [], ayahs: [], order: []),
+  // PageTwelve
+  ElmModelNewOrder(subtitles: [], texts: [], ayahs: [], order: []),
+  // PageTherteen
+  ElmModelNewOrder(subtitles: [], texts: [], ayahs: [], order: []),
+  //
+];
