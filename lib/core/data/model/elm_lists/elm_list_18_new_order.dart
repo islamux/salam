99997118145d98c3d2a3920a00682c260388a6bb
islamux <fathi733l@gmail.com>
2025-05-16 08:43:53 +0300

    repo to gitlab for the first time from terminal

--- lib/core/data/model/elm_lists/elm_list_18_new_order.dart
+++ lib/core/data/model/elm_lists/elm_list_18_new_order.dart
@@ -240,6 +240,302 @@ List<ElmModelNewOrder> elmList18NewOrder = [
     order: [
       EnOrder.texts,
       EnOrder.ayahs,
+      EnOrder.texts,
+    ],
+  ),
+
+  // using copilot gpt4.1
+  // 16
+  ElmModelNewOrder(
+    titles: [
+      ElmTextDersEighteen.titleEighteenSixteen_1,
+      ElmTextDersEighteen.titleEighteenSixteen_2,
+    ],
+    texts: [
+      ElmTextDersEighteen.elmTextEighteenSixteen_1,
+      ElmTextDersEighteen.elmTextEighteenSixteen_2,
+      ElmTextDersEighteen.elmTextEighteenSixteen_3,
+    ],
+    order: [
+      EnOrder.titles,
+      EnOrder.texts,
+      EnOrder.titles,
+      EnOrder.texts,
+      EnOrder.texts,
+    ],
+  ),
+
+  // 17
+  ElmModelNewOrder(
+    titles: [
+      ElmTextDersEighteen.titleEighteenSeventeen_1,
+    ],
+    texts: [
+      ElmTextDersEighteen.elmTextEighteenSeventeen_1,
+      ElmTextDersEighteen.elmTextEighteenSeventeen_2,
+    ],
+    order: [
+      EnOrder.titles,
+      EnOrder.texts,
+      EnOrder.texts,
+    ],
+  ),
+
+  // 18
+  ElmModelNewOrder(
+    titles: [
+      ElmTextDersEighteen.titleEighteenEighteen_1,
+    ],
+    texts: [
+      ElmTextDersEighteen.elmTextEighteenEighteen_1,
+      ElmTextDersEighteen.elmTextEighteenEighteen_2,
+    ],
+    subtitles: [
+      ElmTextDersEighteen.subtitleEighteenEighteen_1,
+    ],
+    order: [
+      EnOrder.titles,
+      EnOrder.texts,
+      EnOrder.subtitles,
+      EnOrder.texts,
+    ],
+  ),
+
+  // 19
+  ElmModelNewOrder(
+    titles: [
+      ElmTextDersEighteen.titleEighteenNineteen_1,
+    ],
+    texts: [
+      ElmTextDersEighteen.elmTextEighteenNineteen_1,
+      ElmTextDersEighteen.elmTextEighteenNineteen_2,
+    ],
+    order: [
+      EnOrder.titles,
+      EnOrder.texts,
+      EnOrder.texts,
+    ],
+  ),
+
+  // 20
+  ElmModelNewOrder(
+    titles: [
+      ElmTextDersEighteen.titleEighteenTwenty_1,
+    ],
+    texts: [
+      ElmTextDersEighteen.elmTextEighteenTwenty_1,
+      ElmTextDersEighteen.elmTextEighteenTwenty_2,
+    ],
+    order: [
+      EnOrder.titles,
+      EnOrder.texts,
+      EnOrder.texts,
+    ],
+  ),
+
+  // 21
+  ElmModelNewOrder(
+    titles: [
+      ElmTextDersEighteen.titleEighteenTwentyOne_1,
+    ],
+    texts: [
+      ElmTextDersEighteen.elmTextEighteenTwentyOne_1,
+      ElmTextDersEighteen.elmTextEighteenTwentyOne_2,
+    ],
+    order: [
+      EnOrder.titles,
+      EnOrder.texts,
+      EnOrder.texts,
+    ],
+  ),
+
+  // 22
+  ElmModelNewOrder(
+    titles: [
+      ElmTextDersEighteen.titleEighteenTwentyTwo_1,
+    ],
+    texts: [
+      ElmTextDersEighteen.elmTextEighteenTwentyTwo_1,
+      ElmTextDersEighteen.elmTextEighteenTwentyTwo_2,
+    ],
+    order: [
+      EnOrder.titles,
+      EnOrder.texts,
+      EnOrder.texts,
+    ],
+  ),
+
+  // 23
+  ElmModelNewOrder(
+    titles: [
+      ElmTextDersEighteen.titleEighteenTwentyThree_1,
+    ],
+    texts: [
+      ElmTextDersEighteen.elmTextEighteenTwentyThree_1,
+      ElmTextDersEighteen.elmTextEighteenTwentyThree_2,
+    ],
+    order: [
+      EnOrder.titles,
+      EnOrder.texts,
+      EnOrder.texts,
+    ],
+  ),
+
+  // 24
+  ElmModelNewOrder(
+    titles: [
+      ElmTextDersEighteen.titleEighteenTwentyFour_1,
+    ],
+    texts: [
+      ElmTextDersEighteen.elmTextEighteenTwentyFour_1,
+      ElmTextDersEighteen.elmTextEighteenTwentyFour_2,
+    ],
+    order: [
+      EnOrder.titles,
+      EnOrder.texts,
+      EnOrder.texts,
+    ],
+  ),
+
+  // 25
+  ElmModelNewOrder(
+    titles: [
+      ElmTextDersEighteen.titleEighteenTwentyFive_1,
+    ],
+    texts: [
+      ElmTextDersEighteen.elmTextEighteenTwentyFive_1,
+      ElmTextDersEighteen.elmTextEighteenTwentyFive_2,
+      ElmTextDersEighteen.elmTextEighteenTwentyFive_3,
+    ],
+    order: [
+      EnOrder.titles,
+      EnOrder.texts,
+      EnOrder.texts,
+      EnOrder.texts,
+    ],
+  ),
+
+  // 26
+  ElmModelNewOrder(
+    titles: [
+      ElmTextDersEighteen.titleEighteenTwentySix_1,
+    ],
+    subtitles: [
+      ElmTextDersEighteen.subtitleEighteenTwentySix_1,
+    ],
+    texts: [
+      ElmTextDersEighteen.elmTextEighteenTwentySix_1,
+    ],
+    order: [
+      EnOrder.titles,
+      EnOrder.subtitles,
+      EnOrder.texts,
+    ],
+  ),
+  ElmModelNewOrder(
+    ayahs: [
+      ElmTextDersEighteen.ayahHadithEighteenTwentySix_1,
+    ],
+    texts: [
+      ElmTextDersEighteen.elmTextEighteenTwentySix_2,
+    ],
+    order: [
+      EnOrder.ayahs,
+      EnOrder.texts,
+    ],
+  ),
+  ElmModelNewOrder(
+    ayahs: [
+      ElmTextDersEighteen.ayahHadithEighteenTwentySix_2,
+    ],
+    texts: [
+      ElmTextDersEighteen.elmTextEighteenTwentySix_3,
+    ],
+    order: [
+      EnOrder.ayahs,
+      EnOrder.texts,
+    ],
+  ),
+  ElmModelNewOrder(
+    texts: [
+      ElmTextDersEighteen.elmTextEighteenTwentySix_4,
+    ],
+    order: [
+      EnOrder.texts,
+    ],
+  ),
+
+  // 27
+  ElmModelNewOrder(
+    titles: [
+      ElmTextDersEighteen.titleEighteenTwentySeven_1,
+    ],
+    texts: [
+      ElmTextDersEighteen.elmTextEighteenTwentySeven_1,
+    ],
+    ayahs: [
+      ElmTextDersEighteen.ayahHadithEighteenTwentySeven_1,
+    ],
+    order: [
+      EnOrder.titles,
+      EnOrder.texts,
+      EnOrder.ayahs,
+    ],
+  ),
+  ElmModelNewOrder(
+    texts: [
+      ElmTextDersEighteen.elmTextEighteenTwentySeven_2,
+      ElmTextDersEighteen.elmTextEighteenTwentySeven_3,
+      ElmTextDersEighteen.elmTextEighteenTwentySeven_4,
+    ],
+    order: [
+      EnOrder.texts,
+      EnOrder.texts,
+      EnOrder.texts,
+    ],
+  ),
+  ElmModelNewOrder(
+    subtitles: [
+      ElmTextDersEighteen.subtitleEighteenTwentySeven_1,
+    ],
+    texts: [
+      ElmTextDersEighteen.elmTextEighteenTwentySeven_5,
+      ElmTextDersEighteen.elmTextEighteenTwentySeven_6,
+      ElmTextDersEighteen.elmTextEighteenTwentySeven_7,
+      ElmTextDersEighteen.elmTextEighteenTwentySeven_8,
+      ElmTextDersEighteen.elmTextEighteenTwentySeven_9,
+      ElmTextDersEighteen.elmTextEighteenTwentySeven_10,
+      ElmTextDersEighteen.elmTextEighteenTwentySeven_11,
+    ],
+    order: [
+      EnOrder.subtitles,
+      EnOrder.texts,
+      EnOrder.texts,
+      EnOrder.texts,
+      EnOrder.texts,
+      EnOrder.texts,
+      EnOrder.texts,
+      EnOrder.texts,
+    ],
+  ),
+  ElmModelNewOrder(
+    texts: [
+      ElmTextDersEighteen.elmTextEighteenTwentySeven_12,
+    ],
+    ayahs: [
+      ElmTextDersEighteen.ayahHadithEighteenTwentySeven_2,
+    ],
+    order: [
+      EnOrder.texts,
+      EnOrder.ayahs,
+    ],
+  ),
+  ElmModelNewOrder(
+    texts: [
+      ElmTextDersEighteen.elmTextEighteenTwentySeven_13,
+    ],
+    order: [
+      EnOrder.texts,
     ],
   ),
 ];
