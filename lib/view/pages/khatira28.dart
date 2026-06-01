9536781b82703f1b8eff72e9500a9a38994179d7
Islamux <fathi733@gmail.com>
2026-06-01 16:51:20 +0300

    refactor(pages): convert khatira24-28 to use KhatiraContentPage (Batch 7)
    Converted 5 more pages. Preserved original title formatting.
    
    Verification: flutter analyze 0 issues.

--- lib/view/pages/khatira28.dart
+++ lib/view/pages/khatira28.dart
@@ -1,13 +1,7 @@
-import 'package:khatir/core/data/model/khatira_lists/khatira_list_28_new_order.dart';
-import 'package:khatir/core/data/static/routes_constant.dart';
-import 'package:khatir/core/data/static/theme/app_color_constant.dart';
-import 'package:khatir/cubit/khatira_cubits/khatira_28_cubit.dart';
-import 'package:khatir/helpers/extensions/navigation_helper.dart';
-import 'package:khatir/view/widget/custom_text_slider/generic_custom_text_slider.dart';
-import 'package:khatir/core/data/static/imagelink/image_link.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:khatir/helpers/search/data_search.dart';
+import 'package:khatir/core/data/model/khatira_lists/khatira_list_28_new_order.dart';
+import 'package:khatir/cubit/khatira_cubits/khatira_28_cubit.dart';
+import 'package:khatir/view/pages/khatira_content_page.dart';
 
 class Khatira28Page extends StatelessWidget {
   final int? initialPage;
@@ -16,88 +10,11 @@ class Khatira28Page extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) {
-        final cubit = Khatira28Cubit();
-        if (initialPage != null && initialPage! > 0) {
-          cubit.goToPageAfterBuild(initialPage!);
-        }
-        return cubit;
-      },
-      child: Builder(
-        builder: (context) {
-          return Scaffold(
-            appBar: AppBar(
-              backgroundColor: AppColor.black,
-              foregroundColor: AppColor.amber,
-              title: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  IconButton(
-                    onPressed: () {
-                      final currentPageIndex =
-                          context.read<Khatira28Cubit>().currentPageIndex;
-                      context.read<Khatira28Cubit>().customShareContent(
-                          currentPageIndex, khatiraList28NewOrder);
-                    },
-                    icon: const Icon(Icons.share),
-                  ),
-                  const Text(
-                    "الخاطرة 28",
-                  ),
-                ],
-              ),
-              centerTitle: true,
-              leading: GestureDetector(
-                onTap: () {
-                  context.read<Khatira28Cubit>().resetCounter();
-                  context.pushNamed(RoutesConstant.home);
-                },
-                child: const Icon(Icons.arrow_back),
-              ),
-              actions: [
-                IconButton(
-                  onPressed: () {
-                    context.read<Khatira28Cubit>().decreaseFontSize();
-                  },
-                  icon: const Icon(Icons.remove),
-                ),
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text("الخط"),
-                  ],
-                ),
-                IconButton(
-                  onPressed: () {
-                    context.read<Khatira28Cubit>().increaseFontSize();
-                  },
-                  icon: const Icon(Icons.add),
-                ),
-                IconButton(
-                  onPressed: () {
-                    showSearch(context: context, delegate: DataSearch());
-                  },
-                  icon: const Icon(Icons.search),
-                ),
-              ],
-            ),
-            body: SafeArea(
-              child: Column(
-                children: [
-                  Expanded(
-                    child: GenericCustomTextSlider(
-                      cubit: context.read<Khatira28Cubit>(),
-                      dataList: khatiraList28NewOrder,
-                      backgroundImagePath: ImageLink.image12,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          );
-        },
-      ),
+    return KhatiraContentPage<Khatira28Cubit>(
+      createCubit: (_) => Khatira28Cubit(),
+      dataList: khatiraList28NewOrder,
+      title: "الخاطرة 28",
+      initialPage: initialPage,
     );
   }
 }
